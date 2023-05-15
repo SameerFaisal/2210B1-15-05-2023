bf68276175b08a121ff11656bb75eeafef7fb985
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-15 11:12:13 +0500

    2210B1 (15-05-2023)

---
+++ app.js
@@ -0,0 +1,31 @@
+//Q1
+// function checkPalindrome(){
+//     let str=document.getElementById("inp").value
+//    let text=""
+//    for(let i=str.length-1;i>=0;i--){
+//          text=text+str[i]
+//    }
+//    if(str.toLowerCase()===text.toLowerCase()){
+//          document.getElementById("ans").innerHTML="Palindrome"
+//    }
+//    else{
+//             document.getElementById("ans").innerHTML="Not Palindrome"
+//    }
+// }
+
+//Q2
+// count=0
+// function increase(){
+//     count++
+//     document.getElementById("val").innerHTML=count
+    
+// }
+
+// function decrease(){
+//     count--
+//     if(count<0){
+//         count=0
+//     }
+//     document.getElementById("val").innerHTML=count
+// }
+
---
+++ oop.js
@@ -0,0 +1,89 @@
+// class Person{
+//     pName
+//     pAge
+//     pCNIC
+// constructor(personName,personAge,personCNIC){
+//     this.pName=personName
+//     this.pAge=personAge
+//     this.pCNIC=personCNIC
+    
+// }
+//     display(){
+//        console.log(`Person Name is: ${this.pName} Age is: ${this.pAge} and CNIC is: ${this.pCNIC}`)
+
+//     }
+// }
+// const p1=new Person("Talha",20,"4201")
+// p1.display()
+// const p2=new Person("Waqas",15,"42201")
+// p2.display()
+// const p3=new Person("Shahood",5,"1258")
+// p3.display()
+// const p4=new Person("Tooba",18,"1259")
+// p4.display()
+// const p5=new Person("Rabia",12,"1256")
+// p5.display()
+// console.log("Person 1 Data")
+// const p1=new Person()
+// p1.pName="Ali"
+// p1.pAge=20
+// p1.pCNIC="123456789"
+// p1.display()
+// console.log("Person 2 Data")
+// const p2=new Person()
+// p2.pName="Sadiq"
+// p2.pAge=25
+// p2.pCNIC="987654321"
+// p2.display()
+// class Car{
+//     carName
+//     carModel
+//     carColor
+//     carPrice
+//     carNumber
+//     constructor(carName,carModel,carColor,carPrice,carNumber){
+//         this.carName=carName
+//         this.carModel=carModel
+//         this.carColor=carColor
+//         this.carPrice=carPrice
+//         this.carNumber=carNumber
+//     }
+//     display(){
+//         console.log(`Car Name is: ${this.carName} Model is: ${this.carModel} Color is: ${this.carColor} and Price is: ${this.carPrice}`)
+//     }
+// }
+
+// const c1=new Car("Civic","2019","Black","2.5 Million","ABC-123")
+// c1.display()
+// const c2=new Car("Mehran",1970,"White",800000,"AAA-111")
+// c2.display()
+// const c3=new Car("Fortuner",2018,"Black",20000000,"BBB-222")
+// c3.display()
+
+// class Car{
+//     name
+//     model
+//     noOfDoors
+//     color
+//     price
+//     constructor(name,model,noOfDoors,color,price){
+// this.name=name
+// this.model=model
+// this.noOfDoors=noOfDoors
+// this.color=color
+// this.price=price
+//     }
+//     display(){
+//         document.write(`Car Name is: ${this.name} Model is: ${this.model} No of Doors are: ${this.noOfDoors} Color is: ${this.color} and Price is: ${this.price}`)
+//     }
+//     startEngine(){
+//         document.write(`${this.name} engine has started <br>`)
+//     }
+//     move(){
+//         document.write(`${this.name} is moving <br>`)
+//     }
+// }
+
+// const c1=new Car("Mehran",1980,4,"Grey","1 million")
+// c1.startEngine()
+// c1.move()
